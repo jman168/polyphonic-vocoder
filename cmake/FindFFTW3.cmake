05fe555ed1d910da08e14901b92bd58274982a7f
Jason <jasonabeckmann@gmail.com>
2021-01-27 08:27:47 -0500

    Added cmake build structure

---
+++ cmake/FindFFTW3.cmake
@@ -0,0 +1,2 @@
+find_path(FFTW3_INCLUDE_DIR NAMES fftw3.h)
+find_library(FFTW3_LIBRARY NAMES fftw3)
---
+++ cmake/FindJACK.cmake
@@ -0,0 +1,2 @@
+find_path(JACK_INCLUDE_DIR NAMES jack/jack.h)
+find_library(JACK_LIBRARY NAMES jack)
